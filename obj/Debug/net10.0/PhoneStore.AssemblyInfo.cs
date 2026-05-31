c29325a0d54f96e50effefe7f4c0786e54d19cb2
Tran Duc Ạnh <trandanh1710@gmail.com>
2026-05-31 20:21:24 +0700

    Cập nhật code mới

--- obj/Debug/net10.0/PhoneStore.AssemblyInfo.cs
+++ obj/Debug/net10.0/PhoneStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhoneStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53883241185e2a4f12fc9a9117c6fcf73c082384")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhoneStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhoneStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
